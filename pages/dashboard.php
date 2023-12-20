34398a4185067bfffa6785fbcca3fba9beeed5b9
Saladii <selehadinseid1@gmail.com>
2023-12-20 12:51:05 +0000

    adding sections to the the dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -17,17 +17,52 @@ $username = $_SESSION['username'];
 <link rel="stylesheet" href="../css/specific/dashboard.css">
 <link rel="stylesheet" href="/css/enhanced.css">
 
+<div class="container-fluid">
     <h1>Welcome, <?php echo $username; ?>!</h1>
-        <div class="dashboard-content">
-            <p>This is your personalized dashboard.</p>
-            <!-- Add your additional dashboard content here -->
-        </div>
 
-        <footer>
-            <p><a href="logout.php">Logout</a></p>
-        </footer>
+    <div class="dashboard-content">
+        <p>This is your personalized dashboard.</p>
+
+        <!-- Additional sections -->
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Store</h5>
+                        <p class="card-text">View and manage store information.</p>
+                        <a href="medicine_list.php" class="btn btn-primary">Go to medicine list</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Pharmacy</h5>
+                        <p class="card-text">Manage pharmacy store.</p>
+                        <a href="store_list.php" class="btn btn-primary">Go to store list</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Damaged Medicines</h5>
+                        <p class="card-text">Handle damaged or expired medicines.</p>
+                        <a href="damage_list.php" class="btn btn-primary">Go to Damaged Medicines list</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End of additional sections -->
+
     </div>
 
+    <footer class="mt-4">
+        <p><a href="logout.php">Logout</a></p>
+    </footer>
+</div>
 
-<?php include 'includes/footer.php'; ?>
+<?php include '../includes/footer.php'; ?>
 
